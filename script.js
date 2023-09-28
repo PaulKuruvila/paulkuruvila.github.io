e33f40bd59e26ec8dd761cbe3babfdcb7d2db22c
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2023-09-28 13:44:01 -0500

    Implement nav dropdown
    Make navbar fully responsive as well

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let dropdownElements = [];
+
+addEventListener("resize", (e) => {
+  if(window.innerWidth > 480 && dropdownElements.length > 0) {
+    if(dropdownElements[2].style.display === 'none') { // reset dropdown icons/navbar options
+      dropdownElements[2].style.display = 'flex';
+      dropdownElements[0].style.display = 'inline-block';
+      dropdownElements[1].style.display = 'none';
+    }
+  } else if (window.innerWidth <= 480 && dropdownElements[2].style.display === 'flex') {
+    // hide navbar options again
+    dropdownElements[2].style.display = 'none';
+  }
+});
+
 const openModal = () => {
   document.getElementById('modal-backdrop').style.display = 'block';
 };
@@ -8,6 +23,21 @@ const closeModal = () => {
   document.getElementById('project-site-btn').style.display = 'none';
   document.getElementById('project-close-btn').style.top = '55%';
   document.getElementById('project-embed').setAttribute('src', '');
+};
+
+const toggleDropdown = (pressed) => {
+  dropdownElements = [document.getElementById('nav-dropdown').children[0],
+  document.getElementById('nav-dropdown').children[1], document.getElementById('nav-options')];
+  console.log(pressed,'was pressed');
+  if(pressed === 'open') {
+    dropdownElements[0].style.display = 'none';
+    dropdownElements[1].style.display = 'inline-block';
+    dropdownElements[2].style.display = 'flex';
+  } else if(pressed === 'close') {
+    dropdownElements[0].style.display = 'inline-block';
+    dropdownElements[1].style.display = 'none';
+    dropdownElements[2].style.display = 'none';
+  }
 };
 
 const viewSite = (link) => {
